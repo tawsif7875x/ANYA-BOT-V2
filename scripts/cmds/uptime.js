497b6c4a9e5a4229eeddb8df062390cc7828d26b
HIMU999FF <166631945+HIMU999FF@users.noreply.github.com>
2024-08-02 18:25:36 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -5,7 +5,7 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = {
   config: {
     name: "uptime",
-    aliases: ["upt"],
+    aliases: ["up"],
     version: "1.0",
     author: "JARiF@Cock",
     role: 0,
